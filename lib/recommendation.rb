f8c9f0fbc124e0f3f95996a9b3bf2a99d7d6183f
id774 <idnanashi@gmail.com>
2013-10-18 16:43:04 +0900

    16 Fix version number

--- lib/recommendation.rb
+++ lib/recommendation.rb
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 module Recommendation
-  VERSION = "0.0.1"
+  VERSION = "0.1.3"
   require File.dirname(__FILE__) +  "/recommendation/supervisor"
   require File.dirname(__FILE__) +  "/recommendation/engine"
 end

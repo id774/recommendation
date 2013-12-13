6c09cc1d9c5520832a19ddcd7ab5f11336a0d732
id774 <idnanashi@gmail.com>
2013-12-13 14:47:38 +0900

    26 Update demo

--- demo.rb
+++ demo.rb
@@ -6,16 +6,15 @@ $:.unshift File.join(File.dirname(__FILE__))
 require 'lib/recommendation'
 
 def demo
-  supervisor = Recommendation::Supervisor.new(visitors)
-  supervisor.train(new_comer)
-  engine = Recommendation::Engine.new
+  recommender = Recommendation::Recommender.new(visitors)
+  recommender.train(new_comer)
 
-  p engine.recommendation(supervisor.table, 'Toby') # => [["The Night Listener", 3.3477895267131017], ["Lady in the Water", 2.8325499182641614], ["Just My Luck", 2.530980703765565]]
+  p Recommendation::Recommender.recommendation(recommender.table, 'Toby') # => [["The Night Listener", 3.3477895267131017], ["Lady in the Water", 2.8325499182641614], ["Just My Luck", 2.530980703765565]]
 
-  p engine.top_matches(supervisor.table, 'Toby') # => [["Lisa Rose", 0.9912407071619299], ["Mick LaSalle", 0.9244734516419049], ["Claudia Puig", 0.8934051474415647], ["Jack Matthews", 0.66284898035987], ["Gene Seymour", 0.38124642583151164]]
+  p Recommendation::Recommender.top_matches(recommender.table, 'Toby') # => [["Lisa Rose", 0.9912407071619299], ["Mick LaSalle", 0.9244734516419049], ["Claudia Puig", 0.8934051474415647], ["Jack Matthews", 0.66284898035987], ["Gene Seymour", 0.38124642583151164]]
 
-  movies = supervisor.transform_table
-  p engine.top_matches(movies, 'Superman Returns') # => [["You, Me and Dupree", 0.6579516949597695], ["Lady in the Water", 0.4879500364742689], ["Snake on the Plane", 0.11180339887498941], ["The Night Listener", -0.1798471947990544], ["Just My Luck", -0.42289003161103106]]
+  movies = recommender.transform_table
+  p Recommendation::Recommender.top_matches(movies, 'Superman Returns') # => [["You, Me and Dupree", 0.6579516949597695], ["Lady in the Water", 0.4879500364742689], ["Snake on the Plane", 0.11180339887498941], ["The Night Listener", -0.1798471947990544], ["Just My Luck", -0.42289003161103106]]
 
 end
 
